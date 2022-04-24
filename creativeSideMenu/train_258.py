034067337e0ec4b031e1ea1864451796d1f4c676
Ascii175 <slimeyilly@gmail.com>
2022-04-25 03:26:32 +0700

    question.h5 upload

--- creativeSideMenu/train_258.py
+++ creativeSideMenu/train_258.py
@@ -34,7 +34,7 @@ mp_holistic = mp.solutions.holistic # Holistic model
 mp_drawing = mp.solutions.drawing_utils # Drawing utilities 
 class train():
         score = 0
-        while 0.9 >= score:
+        while 1 >= score:
             def mediapipe_detection(image, model):
                 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
                 image.flags.writeable = False                  # Image is no longer writeable
@@ -88,10 +88,12 @@ class train():
             #actions = np.array(['1000000'])
 
             # adjust ก 
-            general  = ['nothing','age','Do-you-understand','eat','fine','go','have','how-much',
-            'hungry','me','mhai','miss','name','nevermind','no','now','or','question',
-            'rice','school','sorry','thank-you','time','toothache','what','worry','yang','you']
+            # general  = ['nothing','age','Do-you-understand','eat','fine','go','have','how-much',
+            # 'hungry','me','mhai','miss','name','nevermind','no','now','or','question',
+            # 'rice','school','sorry','thank-you','time','toothache','what','worry','yang','you']
 
+            general  = ['nothing','age','you','how-much','have','question','mhai','Do-you-understand',
+                        'name','what','eat','rice','or','yang','now','time','fine']
             actions = np.array(general)
             # Thirty videos worth of data
             no_sequences = 20
@@ -132,14 +134,14 @@ class train():
             history = model.fit(X_train, y_train, epochs=300, validation_data=(X_test,y_test))
             model.summary()
             res = model.predict(X_test)
-            model.save('sen.h5')
+            model.save('question.h5')
             yhat = model.predict(X_test)
             ytrue = np.argmax(y_test, axis=1).tolist()
             yhat = np.argmax(yhat, axis=1).tolist()
             multilabel_confusion_matrix(ytrue, yhat)
             score = accuracy_score(ytrue, yhat)
             print("accuracy",score)
-            if score < 0.9 :
+            if score < 1 :
                     print("FAIL")
             else :
                     print("SUCCESS")
